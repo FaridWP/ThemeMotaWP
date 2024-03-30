95b469ab2bdfd5f269ea96b2fcf8686e072c01e0
Farid <farid.benmebarek@gmail.com>
2024-03-30 15:18:22 +0100

    jquery activé

--- functions.php
+++ functions.php
@@ -17,6 +17,6 @@ add_action('wp_enqueue_scripts', 'nathaliemota_style');
 // Chargement Scripts JS
 function nathaliemota_script()
 {
-	wp_enqueue_script('mon_script', get_stylesheet_directory_uri() . '/assets/js/scripts.js', array(), '1.0', true);
+	wp_enqueue_script('mon_script', get_stylesheet_directory_uri() . '/assets/js/scripts.js', array('jquery'), '1.0', true);
 }
 add_action('wp_enqueue_scripts', 'nathaliemota_script');
