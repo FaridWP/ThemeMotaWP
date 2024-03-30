f245a5295bb3a7f751c2f9d45624faf0457a9ca9
Farid <farid.benmebarek@gmail.com>
2024-03-30 17:49:08 +0100

    ajout logo et menu config

--- functions.php
+++ functions.php
@@ -20,3 +20,21 @@ function nathaliemota_script()
 	wp_enqueue_script('mon_script', get_stylesheet_directory_uri() . '/assets/js/scripts.js', array('jquery'), '1.0', true);
 }
 add_action('wp_enqueue_scripts', 'nathaliemota_script');
+
+
+function nathaliemota_config()
+{
+	add_theme_support('custom-logo', array(
+		'height' => 22,
+		'width' => 345,
+		'flex-height' => true,
+		'flex-width' => true
+	));
+
+	register_nav_menus(
+		array(
+			'nathaliemota_menu' => 'Main Menu'
+		)
+	);
+}
+add_action('after_setup_theme', 'nathaliemota_config', 0);
--- header.php
+++ header.php
@@ -10,3 +10,25 @@
 </head>
 
 <body <?php body_class(); ?>>
+    <header>
+        <div class="navbar">
+            <div class="logo">
+                <?php
+                if (has_custom_logo()) {
+                    the_custom_logo();
+                } else {
+                ?>
+                    <a href="<?php echo home_url(); ?>"><span><?php bloginfo('name') ?></span></a>
+                <?php
+                }
+                ?>
+            </div>
+
+            <nav class="menu">
+                <ul class="menu-items">
+                    <?php wp_nav_menu(array('theme_location' => 'wp_planty_main_menu', 'depth' => 0)); ?>
+                </ul>
+            </nav>
+        </div>
+
+    </header>
