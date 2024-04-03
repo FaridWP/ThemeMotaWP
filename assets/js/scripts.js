a14b112919ebd6442d6fb10e0baca897ee66ebc2
Farid <farid.benmebarek@gmail.com>
2024-04-03 14:16:15 +0200

    ajout modal

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,3 +3,19 @@ $(".hamburger").on("click", function () {
   $(".hamburger").toggleClass("is-active")
   $(".navbar__menu__mobile").toggleClass("navbar__menu__mobile--active")
 })
+// MENU BURGER CLOSE quand click sur "contact"
+$(".navbar__menu__mobile")
+  .find('a[href="#contact"]')
+  .on("click", function () {
+    $(".hamburger").toggleClass("is-active")
+    $(".navbar__menu__mobile").toggleClass("navbar__menu__mobile--active")
+  })
+
+// MODAL OPEN
+$('a[href="#contact"]').on("click", function () {
+  $(".popup").show()
+})
+// MODAL CLOSE
+$("#overlay").on("click", function () {
+  $(".popup").hide()
+})
--- template-parts/section-modal.php
+++ template-parts/section-modal.php
@@ -9,14 +9,17 @@
  */
 
 ?>
-
-<section id="modal" class="modal">
-    <div class="modal__container">
-        <div class="modal__titre">
-            <h2>Contact</h2>
+<div class="popup">
+    <section id="modal" class="modal">
+        <div class="modal__container">
+            <div class="modal__titre">
+                <img class="modal__img__xl" src="<?php echo get_template_directory_uri() . '/assets/images/Contact_header.png' ?>">
+                <img class="modal__img__xs" src="<?php echo get_template_directory_uri() . '/assets/images/Contact_header_mobile.png' ?>">
+            </div>
+            <div class="modal__form">
+                <?php echo do_shortcode('[contact-form-7 id="585c0be" title="Contact"]'); ?>
+            </div>
         </div>
-        <div class="modal__form">
-            <?php echo do_shortcode('[contact-form-7 id="585c0be" title="Contact"]'); ?>
-        </div>
-    </div>
-</section>
+        <div id="overlay"></div>
+    </section>
+</div>
