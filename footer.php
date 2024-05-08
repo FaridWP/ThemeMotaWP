48291e31dadab3effae43a8b9f2c18f68a62a5fa
Farid <farid.benmebarek@gmail.com>
2024-05-08 12:20:46 +0200

    Ajout Lightbox Scss

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 <footer>
     <!-- Chargement de la Modal -->
     <?php echo get_template_part('template-parts/section', 'modal'); ?>
+    <?php echo get_template_part('template-parts/section', 'lightbox'); ?>
     <div class="footer__menu">
         <ul>
             <?php wp_nav_menu(array('theme_location' => 'footer_menu', 'depth' => 0)); ?>
---
+++ template-parts/section-lightbox.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Template part for displaying section modal
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package NathalieMota
+ */
+?>
+
+<div class="lightbox">
+    <button class="lightbox__close">Fermer</button>
+    <button class="lightbox__next">Suivant</button>
+    <button class="lightbox__prev">Précédent</button>
+    <div class="lightbox__container">
+        <img src="https://picsum.photos/900/1800" alt="">
+    </div>
+</div>
