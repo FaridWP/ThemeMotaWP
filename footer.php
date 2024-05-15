0abac05f3308413d085d759d33168ab8fa0603af
Farid <farid.benmebarek@gmail.com>
2024-05-15 12:22:59 +0200

    fix footer

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
 <footer>
     <!-- Chargement de la Modal -->
     <?php echo get_template_part('template-parts/section', 'modal'); ?>
-    <?php echo get_template_part('template-parts/section', 'lightbox'); ?>
     <div class="footer__menu">
         <ul>
             <?php wp_nav_menu(array('theme_location' => 'footer_menu', 'depth' => 0)); ?>
